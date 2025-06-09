d39e0072d40b1033d53c55fae759593e7788c222
Grace561 <adedayograce58@gmail.com>
2025-06-09 19:05:01 +0100

    04_mathEquations/

--- .history/04_mathEquations/mathEquations_20250609185555.js
+++ .history/04_mathEquations/mathEquations_20250609185555.js
@@ -2,8 +2,8 @@ const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
 const b = 1 + 8
 const c = 22 * 3
 const d = 5 % 4
-const e = b - 17
-const f = a + b + c + d + e 
+const e = "the variable 'b' minus 17
+const f = "the sum of the previous five variables"
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
